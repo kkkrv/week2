b3abe77f60fdebf0462701033e7b86efff4a826e
kkkrv <49373590+kkkrv@users.noreply.github.com>
2020-11-16 17:32:26 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Server } = require('http');
+const fs = require("fs");
+
+Server((req, res) => {
+  res.writeHead(200, {
+		  "Content-Type": "text/plain; charset=UTF-8",
+         	  "Access-Control-Allow-Origin": "*"
+      		});
+  if (req.url === "/login/") {
+		res.end("kokkareva97");
+	} else if (req.url === "/sample/") {
+    function task(x) {
+      return x * this ** 2;
+    }
+		res.end(task.toString());
+	}
+})
+.listen(process.env.PORT);
