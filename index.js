9f5247824434548adaedea8c7d3ef01e635ae7e8
kkkrv <49373590+kkkrv@users.noreply.github.com>
2020-11-20 15:34:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { Server } = require('http');
 const fs = require("fs");
 
+const cors = { 
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+  "Access-Control-Allow-Headers": "x-test,Content-Type,Accept,Access-Control-Allow-Headers"
+};
+
 Server((req, res) => {
   res.writeHead(200, {
       "Content-Type": "text/html",
-      "Access-Control-Allow-Origin": "*"
+      ...cors
   });
   if (req.url === "/login/") {
       res.end("kokkareva97");
@@ -23,12 +29,26 @@ Server((req, res) => {
           }
           res.end();
       });
-  }
-   else if (req.url === "/promise/") {
+  } else if (req.url === "/promise/") {
       function task(x) {
         return new Promise((res, rej) => x < 18 ? res('yes') : rej('no'));
       }
       res.end(task.toString());
+  } else if (req.url === "/promise/") {
+      const result = {
+        message: "kokkareva97",
+        "x-result": req.headers['x-test']
+      }
+      let body = "";
+      req.on('data', chunk => body += chunk)
+      .on('end', () => {
+        res.writeHead('200', {
+          "Content-Type": "application/json", 
+          ...cors
+        })
+        result['x-body'] = body;
+        res.end(JSON.stringify(result));
+      })
   }
 })
 .listen(process.env.PORT);
